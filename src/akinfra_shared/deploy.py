acc844fbf8a1e68dabf0d1548775a3cfaa35cf50
Andreas Kloeckner <inform@tiker.net>
2026-05-27 13:34:01 -0500

    Add nebula deploy

--- src/akinfra_shared/deploy.py
+++ src/akinfra_shared/deploy.py
@@ -1,9 +1,16 @@
+import os
 from io import BytesIO
+from pathlib import Path
+from typing import Any, ClassVar, Literal
+from warnings import warn
 
-from pyinfra import host
+import yaml
+from pydantic import BaseModel, ConfigDict, model_validator
 from pyinfra.api import deploy
+from pyinfra.context import host
 from pyinfra.facts.server import Kernel, LinuxName
 from pyinfra.operations import apt, files, server, systemd
+from typing_extensions import Self
 
 from akinfra_shared.tools import needs_sudo, render_template
 
@@ -173,6 +180,183 @@ def install_default_packages():
         )
 
 
+class NebulaDNSConfig(BaseModel):
+    model_config: ClassVar[ConfigDict] = ConfigDict(extra="forbid")
+
+    host: str
+    port: int
+
+
+class NebulaFirewallConnection(BaseModel):
+    model_config: ClassVar[ConfigDict] = ConfigDict(extra="forbid")
+
+    port: int
+    proto: Literal["any", "tcp", "udp", "icmp"]
+    host: str | None = None
+    group: str | None = None
+
+    @model_validator(mode="after")
+    def check_host_or_group(self) -> Self:
+        if not self.host and not self.group:
+            raise ValueError("host or group must be supplied")
+
+        return self
+
+    @model_validator(mode="after")
+    def check_host(self) -> Self:
+        if self.host and not ("." in self.host or self.host == "any"):
+            raise ValueError("host must contain a dot or be 'any'")
+        return self
+
+    def to_json(self):
+        result: dict[str, Any] = {
+            "port": self.port,
+            "proto": self.proto,
+        }
+
+        if self.host:
+            result["host"] = self.host
+        if self.group:
+            result["group"] = self.group
+        return result
+
+
+class NebulaConfig(BaseModel):
+    model_config: ClassVar[ConfigDict] = ConfigDict(extra="forbid")
+
+    hostname: str
+    am_relay: bool = False
+    am_lighthouse: bool = False
+    dns: NebulaDNSConfig | None = None
+
+    inbound: list[NebulaFirewallConnection]
+
+
+@deploy("Deploy nebula")
+def deploy_nebula():
+    nebula_config = getattr(host.data, "nebula_config", None)
+    if nebula_config is None:
+        return
+    config = NebulaConfig.model_validate(nebula_config)
+
+    nebula_root_dir = os.environ.get("NEBULA_CONFIG")
+    if nebula_root_dir is None:
+        warn("Not deploying nebula, NEBULA_CONFIG unset", stacklevel=1)
+        return
+    nebula_root = Path(nebula_root_dir)
+    del nebula_root_dir
+    if not nebula_root.is_dir():
+        raise RuntimeError("NEBULA_CONFIG does not point to a directory")
+
+    apt.packages(
+        name="Install package",
+        packages=["nebula"],
+        present=True,
+        _sudo=needs_sudo(host),
+    )
+    ca_path = nebula_root / "ca.crt"
+    key_path = nebula_root / f"{config.hostname}.key"
+    cert_path = nebula_root / f"{config.hostname}.crt"
+    files.put(
+        name="Install CA cert",
+        dest="/etc/nebula/ca.crt",
+        src=BytesIO(ca_path.read_bytes()),
+        mode="600",
+        _sudo=needs_sudo(host),
+    )
+    files.put(
+        name="Install host key",
+        dest=f"/etc/nebula/{config.hostname}.key",
+        src=BytesIO(key_path.read_bytes()),
+        mode="600",
+        _sudo=needs_sudo(host),
+    )
+    files.put(
+        name="Install host cert",
+        dest=f"/etc/nebula/{config.hostname}.crt",
+        src=BytesIO(cert_path.read_bytes()),
+        mode="600",
+        _sudo=needs_sudo(host),
+    )
+    config_data: dict[str, Any] = {
+        "pki": {
+            "ca": "/etc/nebula/ca.crt",
+            "cert": f"/etc/nebula/{ config.hostname }.crt",
+            "key": f"/etc/nebula/{ config.hostname }.key",
+        },
+        "static_host_map": {
+            "10.33.0.3": ["49.13.195.233:4242"]
+        },
+        "lighthouse": {
+            "am_lighthouse": config.am_lighthouse,
+            "interval": 60,
+            "hosts": ["10.33.0.3"],
+            "serve_dns": config.dns is not None,
+        },
+        "logging": {
+            # panic, fatal, error, warning, info, or debug. Default is info
+            "level": "info",
+            # json or text formats currently available. Default is text
+            "format": "text",
+        },
+        "relay": (
+            {"am_relay": True}
+            if config.am_relay else
+            {"relays": ["10.33.0.3"], }
+        ),
+        "punchy": {
+            "punch": True,
+        },
+        "listen": {
+            "host": "[::]",
+            "port": 4242,
+        },
+        "tun": {
+            "disabled": False,
+            "dev": "nebula1",
+        },
+        "firewall": {
+            "conntrack": {
+                "tcp_timeout": "12m",
+                "udp_timeout": "3m",
+                "default_timeout": "10m",
+            },
+            "outbound": [
+                {"port": "any", "proto": "any", "host": "any"}
+            ],
+            "inbound": [
+                {"port": "any", "proto": "icmp", "host": "any"},
+                {"port": 22, "proto": "tcp", "host": "any"},
+                *[ib.to_json() for ib in config.inbound]
+            ],
+        },
+    }
+    if config.dns is not None:
+        config_data["lighthouse"]["dns"] = config.dns.model_dump()
+    config_store_op = files.put(
+        name="Write config file",
+        dest=f"/etc/nebula/{config.hostname}.yml",
+        src=BytesIO(yaml.dump(config_data, Dumper=yaml.Dumper).encode()),
+        _sudo=needs_sudo(host),
+    )
+    service_name = f"nebula@{config.hostname}"
+    systemd.service(
+        name="Install/start nebula daemon",
+        service=service_name,
+        enabled=True,
+        running=True,
+        reloaded=True,
+        _sudo=needs_sudo(host),
+    )
+    systemd.service(
+        name="Restart nebula daemon",
+        service=service_name,
+        restarted=True,
+        _if=config_store_op.did_change,
+        _sudo=needs_sudo(host),
+    )
+
+
 def all():
     mitigate_copyfail()
     mitigate_dirtyfrag()
@@ -180,3 +364,4 @@ def all():
     install_apt_sources()
     install_default_packages()
     set_up_network_dhcp()
+    deploy_nebula()
