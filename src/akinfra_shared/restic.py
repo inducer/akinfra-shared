b52d356ac16988fe52639b7850d230f9348cf8c5
Andreas Kloeckner <inform@tiker.net>
2026-07-02 11:50:36 -0500

    Restic: multiple targets, certs from bw, allow_users

--- src/akinfra_shared/restic.py
+++ src/akinfra_shared/restic.py
@@ -1,11 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from io import BytesIO
 
 from pyinfra.api.deploy import deploy
 from pyinfra.context import host
 from pyinfra.operations import apt, files
 
-from akinfra_shared.tools import get_bitwarden_password, render_template
+from akinfra_shared.tools import get_bitwarden_notes, get_bitwarden_password, render_template
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -15,44 +15,31 @@ class ResticRoot:
 
 
 @dataclass(frozen=True, kw_only=True)
-class ResticConfig:
+class ResticServerAuthInfo:
     user: str
+    password_id: str
+
+
+@dataclass(frozen=True, kw_only=True)
+class ResticTarget:
+    name: str
+    cert_id: str
+
+    server_auth: ResticServerAuthInfo | None
+
     host: str
     path: str
-
-    password_id: str
-    repo_password_id: str
-
-    roots: list[ResticRoot]
     port: int = 8000
 
+    password_id: str
 
-RESTIC_CERT = """
------BEGIN CERTIFICATE-----
-MIID9DCCAtygAwIBAgIUY21my1hwK+Kun6rgtY0t2M9gYAAwDQYJKoZIhvcNAQEL
-BQAwfjELMAkGA1UEBhMCVVMxEDAOBgNVBAgMB0lsaW5vaXMxEjAQBgNVBAcMCUNo
-YW1wYWlnbjEXMBUGA1UECgwOVGlrZXIubmV0IEdtYkgxDzANBgNVBAsMBkJhY2t1
-cDEfMB0GCSqGSIb3DQEJARYQaW5mb3JtQHRpa2VyLm5ldDAeFw0yNDA1MTgxOTMx
-MzBaFw0zNDA1MTYxOTMxMzBaMH4xCzAJBgNVBAYTAlVTMRAwDgYDVQQIDAdJbGlu
-b2lzMRIwEAYDVQQHDAlDaGFtcGFpZ24xFzAVBgNVBAoMDlRpa2VyLm5ldCBHbWJI
-MQ8wDQYDVQQLDAZCYWNrdXAxHzAdBgkqhkiG9w0BCQEWEGluZm9ybUB0aWtlci5u
-ZXQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCm8/+cQBGHcek+ZqCn
-ZFYjXJrT5MoJSWMI0NRduI8EateGZkuW6THsZsHZ1wpHMyCqTGV2J7VJaNIsQwsg
-WTH9P8aQwgAf3yJqVTeu9jbQLx3iGfWEuR11dKmffk+pT7yM2NpaFCJca8egajJ8
-Bz+0dNCiG8e8By4jTtxpt+taOulaT4f9KupQiXdhBswlkOeS6jopbD7fGgfLfYsB
-WI1qn2hZrCR1YQCuw8CifsE/CUVLhJaYOQvPZhxybEOD2OmfOPXUm6ibNrSGQ+t6
-aQXFd3eUJN7PACKtbbHjhY8O2vvztVmstVN5F/lshcwqcSuN4ZP7PoeTKBRHXsGI
-nlZFAgMBAAGjajBoMB0GA1UdDgQWBBT2cv3msrrzB6+FRRkZ6lXV8P74WzAfBgNV
-HSMEGDAWgBT2cv3msrrzB6+FRRkZ6lXV8P74WzAPBgNVHRMBAf8EBTADAQH/MBUG
-A1UdEQQOMAyHBH8AAAGHBAohAgIwDQYJKoZIhvcNAQELBQADggEBACKqoyczcmDF
-tthpPrAfc+DDq0hXkG/wKX/WT4ird8PAFe6q4ZJBIqewqYqNcIlIC4nkmavJWhhX
-GzVjl6YIAPMWQvEv5NvszWYMowDnZ6tLJZ/hOfGSQvHQBCfHOPgHTNtMhB+oAYVy
-YC4JvqVscTQ/Pp+8v3/8bPecV0bxFv3jgocDIAfRzVjBgvC40zTnKpHmYOZ14PhB
-eRAPNqks/p5RwCu5SzW4XHluRL5upUGIMCj+kdcM/HrDf6kD+Bsy7F9ByIjGgPzA
-bXzYILKbIH/c4S5Qr1dKfXpm2krGRe12oynVT+9Z8jZV/zOYAjKRTciql+xyIzlM
-T5bhZSxCzY4=
------END CERTIFICATE-----
-"""
+    allow_users: list[str] = field(default_factory=list)
+
+
+@dataclass(frozen=True, kw_only=True)
+class ResticConfig:
+    targets: list[ResticTarget]
+    roots: list[ResticRoot]
 
 
 RESTIC_GENERIC_EXCLUDES = [
@@ -80,21 +67,72 @@ def deploy_restic_backup() -> None:
     config = host.data.restic_config
     assert isinstance(config, ResticConfig)
 
+    backup_script = "/etc/restic-backup.sh"
     apt.packages(
         name="Install package",
         packages=["restic"],
     )
+    default_target: str | None = None
+    target_to_url: dict[str, str] = {}
+    for target in config.targets:
+        access = ""
+        auth = target.server_auth
+        if auth:
+            server_pw_file = f"/etc/restic-repo-password-{target.name}"
+            access = f"{auth.user}:$(cat {server_pw_file})@"
+            files.put(
+                name=f"Store repo password for target {target.name}",
+                dest=server_pw_file,
+                mode="600",
+                src=BytesIO(get_bitwarden_password(
+                    auth.password_id).encode()),
+            )
+        target_to_url[target.name] = (
+            f"rest:https://{access}{target.host}:{target.port}{target.path}")
+        if default_target is None:
+            default_target = target.name
+
+        files.put(
+            name="Store password",
+            dest=f"/etc/restic-password-{target.name}",
+            mode="600",
+            src=BytesIO(get_bitwarden_password(
+                target.password_id).encode()),
+        )
+        files.put(
+            name=f"Store restic cert pub key for target {target.name}",
+            dest=f"/etc/restic-public-key-{target.name}.pem",
+            mode="644",
+            src=BytesIO(get_bitwarden_notes(target.cert_id).encode()),
+        )
+
+        sudoers_file = f"/etc/sudoers.d/restic-backup-{target.name}"
+        if target.allow_users:
+            lines = "\n".join(
+                f"{user} ALL=(ALL) NOPASSWD: {backup_script} {target.name}"
+                for user in target.allow_users
+            ) + "\n"
+            files.put(
+                name=f"Store allow users for target {target.name}",
+                dest=sudoers_file,
+                mode="440",
+                src=BytesIO(lines.encode()),
+            )
+        else:
+            files.file(
+                path=sudoers_file,
+                present=False,
+            )
+    assert default_target
+
     script_content = render_template(
         "restic-backup.sh.jinja",
         module_name="akinfra_shared",
         template_vars={
-            "user": config.user,
-            "host": config.host,
-            "port": config.port,
-            "path": config.path,
+            "target_to_url": target_to_url,
+            "default_target": default_target,
             "roots": config.roots,
         })
-    backup_script = "/etc/restic-backup.sh"
     files.put(
         name="Store backup script",
         dest=backup_script,
@@ -106,24 +144,4 @@ def deploy_restic_backup() -> None:
         # NB: Cannot have a file extension, or else it won't get run.
         path="/etc/cron.daily/restic-backup",
         target=backup_script,
-    )
-    files.put(
-        name="Store password",
-        dest="/etc/restic-password",
-        mode="600",
-        src=BytesIO(get_bitwarden_password(
-            config.password_id).encode()),
-    )
-    files.put(
-        name="Store repo password",
-        dest="/etc/restic-repo-password",
-        mode="600",
-        src=BytesIO(get_bitwarden_password(
-            config.repo_password_id).encode()),
-    )
-    files.put(
-        name="Store restic cert pub key",
-        dest="/etc/restic-public-key.pem",
-        mode="644",
-        src=BytesIO(RESTIC_CERT.encode()),
     )
